7e73989a4816b63544135fe7744d23b53ee326c8
Mgamboa <marianellag400@gmail.com>
2022-09-21 16:21:30 -0500

    modified READ.ME instructions to have everything sorted and I am aware of each step I need to do. Also started with packages needed for the challenge, as well as structure for questions with inquirer.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,45 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name',
+
+        },
+        {
+            type: 'input',
+            message: 'Where are you located?',
+            name: 'location',
+
+        },
+        {
+            type: 'input',
+            message: 'How do you describe yourself?',
+            name: 'bio',
+
+        },
+        {
+            type: 'input',
+            message: 'What is your LinkedIn?',
+            name: 'LinkedIn',
+
+        },
+        {
+            type: 'input',
+            message: 'What is your Github username?',
+            name: 'GitHub',
+
+        },
+    ])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();

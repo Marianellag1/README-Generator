9b2ca8699f185e9656507cc7cf1a87552ce377c9
Mgamboa <marianellag400@gmail.com>
2022-09-22 14:45:10 -0500

    removed tabs from template

--- build/README.md
+++ build/README.md
@@ -1,39 +1,38 @@
 
-        # **afsf**
-        
-        ### **License Application is Covered Under:**
-            MIT 
-        ------------------------------------------------------------
-        
-        # **Table of Contents**
-        
-        -[Installation Instructions](#installation-instructions)
-        #<a name="installation"></a> Installation
-        -[Usage Information](#usage-information)
-        -[License](#license)
-        -[Contributing Guidelines](#contributing-guidelines)
-        -[Test Instructions](#test-instructions)
-        -[Question](#question)
-        
-        ------------------------------------------------------------
-        ## **Description of the Project**
-             dfasdf
+# **afsf**
 
-        ## **Installation**
-            dfsdf
+### **License Application is Covered Under:**
+    MIT 
+------------------------------------------------------------
 
-        ## **Usage Information**
-            fdsafd
+# **Table of Contents**
+
+-[Installation Instructions](#installation-instructions)
+#<a name="installation"></a> Installation
+-[Usage Information](#usage-information)
+-[License](#license)
+-[Contributing Guidelines](#contributing-guidelines)
+-[Test Instructions](#test-instructions)
+-[Question](#question)
+
+------------------------------------------------------------
+## **Description of the Project**
+        dfasdf
+
+## **Installation**
+    dfsdf
+
+## **Usage Information**
+    fdsafd
 
 
-        ## **Contribution Guidelines**
-            dfasdf
+## **Contribution Guidelines**
+    dfasdf
 
-        ## **Test Insctructions**
-             sdfsdf
+## **Test Insctructions**
+        sdfsdf
 
-        ## **Questions?**
-        ### You can reach me on:
-            Github:<https://www.github.com/fasdf>
-            E-mail:<sdfsdf>
-        
+## **Questions?**
+### You can reach me on:
+    Github:<https://www.github.com/fasdf>
+    E-mail:<sdfsdf>
--- readMeGen.js
+++ readMeGen.js
@@ -1,43 +1,43 @@
 const genReadMe = function (title, description, installation, usage, license, contribution, test, github, email) {
     return `
-        # **${title}**
-        
-        ### **License Application is Covered Under:**
-            ${license}
-        ------------------------------------------------------------
-        
-        # **Table of Contents**
-        
-        -[Installation Instructions](#installation-instructions)
-        #<a name="installation"></a> Installation
-        -[Usage Information](#usage-information)
-        -[License](#license)
-        -[Contributing Guidelines](#contributing-guidelines)
-        -[Test Instructions](#test-instructions)
-        -[Question](#question)
-        
-        ------------------------------------------------------------
-        ## **Description of the Project**
-             ${description}
+# **${title}**
 
-        ## **Installation**
-            ${installation}
+### **License Application is Covered Under:**
+    ${license}
+------------------------------------------------------------
 
-        ## **Usage Information**
-            ${usage}
+# **Table of Contents**
+
+-[Installation Instructions](#installation-instructions)
+#<a name="installation"></a> Installation
+-[Usage Information](#usage-information)
+-[License](#license)
+-[Contributing Guidelines](#contributing-guidelines)
+-[Test Instructions](#test-instructions)
+-[Question](#question)
+
+------------------------------------------------------------
+## **Description of the Project**
+        ${description}
+
+## **Installation**
+    ${installation}
+
+## **Usage Information**
+    ${usage}
 
 
-        ## **Contribution Guidelines**
-            ${contribution}
+## **Contribution Guidelines**
+    ${contribution}
 
-        ## **Test Insctructions**
-             ${test}
+## **Test Insctructions**
+        ${test}
 
-        ## **Questions?**
-        ### You can reach me on:
-            Github:<https://www.github.com/${github}>
-            E-mail:<${email}>
-        `
+## **Questions?**
+### You can reach me on:
+    Github:<https://www.github.com/${github}>
+    E-mail:<${email}>
+`
 }
 
 module.exports = genReadMe;
